95ec2f212aab5551362fbafc619c24da9f0e23f2
Augustine17 <augmen173@gmail.com>
2024-01-12 20:27:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//get div
+const box = document.getElementById("root");
+
+//Change the background color of the fetched div element to yellow using the style.backgroundColor property. 
+box.style.backgroundColor="yellow"
+
+//Add a margin of 20 pixels to the element using the style.margin property.
+box.style.margin = "20px"
+
+//Apply a padding of 10 pixels using the style.padding property.
+box.style.padding = "10px"
+
+//Change the font size to 18 pixels using the style.fontSize property.
+box.style.fontSize = "18px"
+
+//Set the font weight to bold using the style.fontWeight property.
+box.style.fontWeight = "bold"
+
+//Change the height of the element to 200 pixels using the style.height property.
+box.style.height = "200px"
+
+//Modify the width of the element to 300 pixels using the style.width property.
+box.style.width = "300px"
